08078a967e3f39e7790d9e1ea75301bf8919aee4
zar4za <zar4z4@yandex.ru>
2022-03-07 17:58:03 +0300

    Added several BitSkinsClient methods

--- Steam/Clients/BitSkinsClient.cs
+++ Steam/Clients/BitSkinsClient.cs
@@ -22,35 +22,56 @@ namespace Grecatech.Steam.Clients
         }
         #endregion
 
-        public BitSkinsClient(HttpClient httpClient)
+        public BitSkinsClient(HttpClient httpClient, string apiKey)
         {
             _client = httpClient;
+            _apiKey = apiKey;
         }
         private const string RootUrl = "https://bitskins.com/api/v1";
-        private HttpClient _client;
-        public async Task<decimal> GetAccountBalanceAsync(string apiKey, string code)
+        private readonly HttpClient _client;
+        private readonly string _apiKey;
+        private string _twoFactorCode => throw new NotImplementedException();
+        public async Task<decimal> GetAccountBalanceAsync()
         {
-            var url = new Uri($"{RootUrl}/get_account_balance/?api_key={apiKey}&code={code}");
+            var url = new Uri($"{RootUrl}/get_account_balance/?api_key={_apiKey}&code={_twoFactorCode}");
             string response = await _client.GetStringAsync(url);
 
-            //  response json
-            //  {
-            //      available_balance: '13370.420',
-            //      pending_withdrawals: '0.0000',
-            //      withdrawable_balance: '13370.420',
-            //      couponable_balance: '13370.420'
-            //  }
+            /*  RESPONSE JSON
+             *  
+             *  {
+             *      "status": "success",
+             *      "data": {
+             *          "available_balance": "0.0000",
+             *          "pending_withdrawals": "0.0000",
+             *          "withdrawable_balance": "0.0000",
+             *          "couponable_balance": "0.0000"
+             *      }
+             *  }
+             */
 
             JObject balance = JObject.Parse(response);
             return Convert.ToDecimal(balance["available_balance"]);
         }
 
-        public async void GetAllItemPricesAsync(string apiKey, string code, string appId)
+        public async void GetAllItemPricesAsync(string appId)
         {
-            var url = new Uri($"{RootUrl}/get_all_item_prices/?api_key={apiKey}&code={code}&app_id={appId}");
+            var url = new Uri($"{RootUrl}/get_all_item_prices/?api_key={_apiKey}&code={_twoFactorCode}&app_id={appId}");
             string response = await _client.GetStringAsync(url);
-
-
         }
+
+        public async void GetMarketDataAsync(string appId)
+        {
+            var url = new Uri($"{RootUrl}/get_price_data_for_items_on_sale/?api_key={_apiKey}&code={_twoFactorCode}&app_id={appId}");
+            string response = await _client.GetStringAsync(url);
+        }
+        public async void GetAccountInventoryAsync(string appId, int page = 1)
+        {
+            var url = new Uri($"{RootUrl}/get_my_inventory/?api_key={_apiKey}&code={_twoFactorCode}&page={page}&app_id={appId}");
+            string response = await _client.GetStringAsync(url);
+        }
+
+        //TODO: GetInventoryOnSell
+
+
     }
 }
