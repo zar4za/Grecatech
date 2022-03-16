74ec65a18214c9b37e4a0db2e6b5d11b3b91d174
zar4za <zar4z4@yandex.ru>
2022-03-16 21:16:49 +0300

    Renamed Login models for better readability

---
+++ Steam/Models/LoginRequestContent.cs
@@ -0,0 +1,34 @@
+﻿using Grecatech.Steam.Encryption;
+using Grecatech.Steam.Security;
+
+namespace Grecatech.Steam.Models
+{
+    internal class LoginRequestContent
+    {
+        public LoginRequestContent(string username, RSAPassword password, Captcha captcha = null!, SteamGuardCode guard = null!)
+        {
+            string timestamp = DateTimeOffset.Now.ToUnixTimeMilliseconds().ToString();
+            _postContent = new Dictionary<string, string>()
+            {
+                { "donotcache", timestamp },
+                { "password", password.EncodedString },
+                { "username", username },
+                { "twofactorcode", guard?.ToString() ?? string.Empty },
+                { "emailauth", string.Empty },
+                { "loginfriendlyname", string.Empty },
+                { "captchagid", captcha?.Gid ?? string.Empty },
+                { "captcha_text", captcha?.Answer ?? string.Empty },
+                { "emailsteamid", string.Empty },
+                { "rsatimestamp", password.Timestamp },
+                { "remember_login", false.ToString() },
+                { "tokentype", (-1).ToString() }
+            };
+        }
+
+        private Dictionary<string, string> _postContent;
+        public HttpContent ToHttpContent()
+        {
+            return new FormUrlEncodedContent(_postContent);
+        }
+    }
+}
---
+++ Steam/Models/LoginResponseContent.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Grecatech.Steam.Models
+{
+    internal class LoginResponseContent
+    {
+        [JsonPropertyName("success")]
+        public readonly bool Success;
+
+        [JsonPropertyName("message")]
+        public readonly string Message;
+
+        [JsonPropertyName("requires_twofactor")]
+        public readonly bool TwoFactor;
+
+        [JsonPropertyName("captcha_needed")]
+        public readonly bool CaptchaNeeded;
+
+        [JsonPropertyName("captcha_gid")]
+        public readonly string CaptchaGid;
+
+        [JsonPropertyName("emailsteamid")]
+        public readonly string EmailSteamId;
+
+        [JsonConstructor]
+        public LoginResponseContent(bool success, string message, bool twoFactor, bool captchaNeeded, string captchaGid, string emailSteamId)
+        {
+            Success = success;
+            Message = message;
+            TwoFactor = twoFactor;
+            CaptchaNeeded = captchaNeeded;
+            CaptchaGid = captchaGid;
+            EmailSteamId = emailSteamId;
+        }
+    }
+}
