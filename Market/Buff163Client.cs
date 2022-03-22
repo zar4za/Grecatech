04dce803c2056514ebf67e48ed26b5381d341f1b
zar4za <zar4z4@yandex.ru>
2022-03-22 22:56:12 +0300

    Replaced Newtosoft.Json with System.Text.Json Added SellItemAsync method Refactored httpmessage generation

--- Market/Buff163Client.cs
+++ Market/Buff163Client.cs
@@ -1,8 +1,12 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Grecatech.Market.Models;
+using System.Globalization;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace Grecatech.Market
 {
-    public class Buff163Client : IMarketClient
+    public class Buff163Client
     {
         public readonly decimal Fees = 0.975m;
 
@@ -10,22 +14,22 @@ namespace Grecatech.Market
         private const string RootUrl = "https://buff.163.com/api";
 
         private readonly string _session;
-
-        private Dictionary<string, int> _itemIds;
-        private HttpClient _httpClient;
+        private readonly Dictionary<string, int> _itemIds;
+        private readonly HttpClient _httpClient;
+        private readonly CookieCollection _cookies;
 
         public Buff163Client(HttpClient client, string session, string itemIdPath)
         {
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+
             _session = session;
             _httpClient = client;
+            _cookies = new CookieCollection()
+            {
+                new Cookie("session", _session)
+            };
 
-            //Cookie Authorization
-            var cookieUrl = new Uri("https://buff.163.com/");
-            var request = new HttpRequestMessage(HttpMethod.Get, cookieUrl);
-            request.Headers.Add("Cookie", $"session={_session}");
-            _httpClient.SendAsync(request);
-
-            _itemIds = JObject.Parse(File.ReadAllText(itemIdPath)).ToObject<Dictionary<string, int>>();
+            _itemIds = JsonSerializer.Deserialize<Dictionary<string, int>>(File.ReadAllText(itemIdPath))!;
         }
 
         private static long Nonce => DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
@@ -34,12 +38,11 @@ namespace Grecatech.Market
         {
             var url = new Uri($"{RootUrl}/asset/get_brief_asset/?_={Nonce}");
             var response = await _httpClient.GetStringAsync(url);
-            var json = JObject.Parse(response);
+            var json = JsonNode.Parse(response)!.AsObject();
 
-            if(!json.ContainsKey("code") || json["code"].Value<string>() != "OK")
-                return decimal.Zero;
+            EnsureCodeOk(json, "Не удалось получить состояние баланса.");
 
-            var cny = json["data"]["alipay_amount"].Value<decimal>();
+            var cny = json["data"]!["alipay_amount"]!.GetValue<decimal>();
 
             return await ConvertToUsd(cny);
         }
@@ -52,12 +55,11 @@ namespace Grecatech.Market
             var id = _itemIds[marketHashName];
             var url = new Uri($"{RootUrl}/market/goods/sell_order?game=csgo&goods_id={id}&page_num=1&sort_by=price.asc&mode=&allow_tradable_cooldown=0&_={Nonce}");
             var response = await _httpClient.GetStringAsync(url);
-            var json = JObject.Parse(response);
+            var json = JsonNode.Parse(response)!.AsObject();
 
-            if (!json.ContainsKey("code") || json["code"].Value<string>() != "OK")
-                return decimal.Zero;
+            EnsureCodeOk(json, $"Не удалось получить цену {marketHashName}");
 
-            var cny = json["data"]["items"][0]["price"].Value<decimal>();
+            var cny = json["data"]!["items"]![0]!["price"]!.GetValue<decimal>();
 
             return await ConvertToUsd(cny);
         }
@@ -67,29 +69,119 @@ namespace Grecatech.Market
             throw new NotImplementedException();
         }
 
-        //POST: https://buff.163.com/market/sell_order/preview/manual_plus
-        public async Task<string?> SellItemAsync(string marketHashName, decimal price)
+        public async Task<Item> SellItemAsync(Item item, decimal price)
         {
-            throw new NotImplementedException();
+            var cnyPrice = Math.Round(price / await GetUsdConvertRateAsync(), 2) - 0.01m;
+            var url = new Uri("https://buff.163.com/api/market/sell_order/create/manual_plus");
+            var buffId = _itemIds[item.MarketHashName];
+            var income = await GetIncomeAsync(cnyPrice);
+
+            var itemJson = new JsonObject
+            {
+                { "game", "csgo" },
+                { "market_hash_name", item.MarketHashName },
+                { "contextid", 2 },
+                { "assetid", item.AssetId },
+                { "classid", item.ClassId },
+                { "instanceid", item.InstanceId },
+                { "goods_id", buffId },
+                { "price", cnyPrice },
+                { "income", income },
+                { "has_market_min_price", false },
+                { "cdkey_id", string.Empty }
+            };
+            var assets = new JsonArray
+            {
+                itemJson
+            };
+            var payload = new JsonObject
+            {
+                { "game", "csgo" },
+                { "assets", assets }
+            };
+
+            var request = CreateRequest(url, payload);
+
+            var response = await _httpClient.SendAsync(request);
+            var json = JsonNode.Parse(await response.Content.ReadAsStringAsync())!.AsObject();
+
+            EnsureCodeOk(json, "Не удалось продать предмет.");
+            SetCookies(response);
+
+            return item;
         }
 
         public async Task<decimal> GetUsdConvertRateAsync()
         {
             var url = new Uri($"https://buff.163.com/account/api/user/info?_={Nonce}");
             var response = await _httpClient.GetStringAsync(url);
-            var json = JObject.Parse(response);
+            var json = JsonNode.Parse(response)!.AsObject();
 
-            if (!json.ContainsKey("code") || (json["code"].Value<string>() != "OK"))
-                return decimal.Zero;
+            EnsureCodeOk(json, "Не удалось получить курс валюты.");
 
-            var rate = json["data"]["buff_price_currency_rate_base_cny"].Value<decimal>();
+            var rate = json["data"]!["buff_price_currency_rate_base_cny"]!.GetValue<decimal>();
             return rate;
+        }
+
+        private static void EnsureCodeOk(JsonObject json, string exceptionMessage)
+        {
+            if (!json.ContainsKey("code") || (json["code"]!.GetValue<string>() != "OK"))
+                throw new Exception(exceptionMessage);
+        }
+
+        private async Task<decimal> GetIncomeAsync(decimal price)
+        {
+            var url = new Uri($"{RootUrl}/market/batch/fee?game=csgo&prices={price}&is_change=0&check_price=1&_={Nonce}");
+
+            var request = CreateRequest(url);
+            var response = await _httpClient.SendAsync(request);
+            var content = await response.Content.ReadAsStringAsync();
+            var json = JsonNode.Parse(content)!.AsObject();
+
+            EnsureCodeOk(json, "Невозможно проверить комиссию.");
+            SetCookies(response);
+
+            var fee = json["data"]!["fees"]![0]!.GetValue<decimal>();
+            return price - fee;
         }
 
         private async Task<decimal> ConvertToUsd(decimal cny)
         {
             var rate = await GetUsdConvertRateAsync();
             return Math.Round(cny * rate, PointDigits);
+        }
+
+        private HttpRequestMessage CreateRequest(Uri url)
+        {
+            var cookieString = string.Join("; ", _cookies);
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            request.Headers.Add("Cookie", cookieString);
+            return request;
+        }
+
+        private HttpRequestMessage CreateRequest(Uri url, JsonObject json)
+        {
+            var cookieString = string.Join("; ", _cookies);
+            var content = new StringContent(json.ToJsonString());
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            var token = _cookies.SingleOrDefault(c => c.Name == "csrf_token")!.Value.Split(';')[0];
+            request.Headers.Add("X-CSRFToken", token);
+            request.Headers.Add("Cookie", cookieString);
+            request.Headers.Add("Referer", "https://buff.163.com/market/steam_inventory?game=csgo");
+            request.Content = content;
+            return request;
+        }
+
+        private void SetCookies(HttpResponseMessage message)
+        {
+            var setCookies = message.Headers.Single(h => h.Key == "Set-Cookie").Value;
+            foreach (var setCookie in setCookies)
+            {
+                var cookie = setCookie.Split(';')[0].Split('=');
+                _cookies.Add(new Cookie(cookie[0], cookie[1]));
+            }
         }
     }
 }
